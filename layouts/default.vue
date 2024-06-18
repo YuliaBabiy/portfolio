416a5445678c50ff31ede87c5a0c353db6ddcff2
julia <yulia.babiy.1994@gmail.com>
2024-06-18 11:04:00 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <nav class="container flex gap-5">
+        <h2><NuxtLink to="/">Logo</NuxtLink></h2>
+        <ul class="flex gap-5">
+          <li><NuxtLink to="/">About</NuxtLink></li>
+          <li><NuxtLink to="/">Projects</NuxtLink></li>
+        </ul>
+        <button class="custom-class">Let's talk</button>
+      </nav>
+    </header>
+    <div class="container">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
